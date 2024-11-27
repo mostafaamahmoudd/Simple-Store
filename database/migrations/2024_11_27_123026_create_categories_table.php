76f2cc8e83c9e9ee926c8d88af2c821496d3ea97
mostafaamahmoudd <mostafamahmoud805554@std.mans.edu.eg>
2024-11-27 15:13:13 +0200

    Database and Migrations

--- database/migrations/2024_11_27_123026_create_categories_table.php
+++ database/migrations/2024_11_27_123026_create_categories_table.php
@@ -11,14 +11,16 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('parent_it')->nullable()
+                ->constrained('categories', 'id')
+                ->nullOnDelete();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('phone_number');
-            $table->rememberToken();
+            $table->string('slug')->unique();
+            $table->string('image')->nullable();
+            $table->text('description')->nullable();
+            $table->enum('status', ['active', 'archived']);
             $table->timestamps();
         });
     }
@@ -28,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('categories');
     }
 };
